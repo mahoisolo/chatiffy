21cc246f031589dd61fdbf68b2628d2011604438
mahoi <mahiisolloo@gmail.com>
2025-11-25 09:56:23 +0300

    login and logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -74,3 +74,30 @@ export const signup = async (req, res) => {
     return res.status(500).json({ message: "Server error" });
   }
 };
+export const login=async(req,res)=>{
+  const {email,password}=req.body;
+  try {
+    const user=await User.findOne({email});
+    if(!user) return res.status(400).json({message:"invalid credentials"});
+    const isPasswordCorrect=await bcrypt.compare(password,user.password);
+    if(!isPasswordCorrect)return res.status(400).json({message:"invalid credentials"});
+    generateToken(user._id,res);
+    res.status(200).json({
+        message:"Login successful",
+        user:{
+            _id:user._id,
+            fullName:user.fullName,
+            email:user.email,
+            profilepic:user.profilepic
+        }
+    });
+  } catch (error) {
+    console.error("Error in login controller",error);
+    return res.status(500).json({message:"Server error"});
+  }
+}
+export const logout=(_,res)=>{
+  res.cookie('jwt','',{maxAge:0});
+  res.status(200).json({message:"Logout successful"});
+
+}
